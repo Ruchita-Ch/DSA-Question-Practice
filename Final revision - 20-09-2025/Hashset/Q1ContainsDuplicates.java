17a0f6257110ac4adc21221d2aa3567272caa394
ru2003 <ruchitanchaudhari145@gmail.com>
2026-01-23 02:23:55 +0530

    new changes

--- Final revision - 20-09-2025/Hashset/Q1ContainsDuplicates.java
+++ Final revision - 20-09-2025/Hashset/Q1ContainsDuplicates.java
@@ -9,6 +9,9 @@ Output → true */
 import java.util.HashSet;
 public class Q1ContainsDuplicates {
     public static void main(String[] args){
+          int[] arr= {1,2,3,4,1,5,6,9};
 
+          HashSet<Integer> set = new HashSet<>();
+          
     }
 }
