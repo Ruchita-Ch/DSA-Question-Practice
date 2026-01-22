ace5583ff1799931f0041aa5c0c5dbe5ee5a6a7f
ru2003 <ruchitanchaudhari145@gmail.com>
2026-01-23 02:22:50 +0530

    new changes

--- Final revision - 20-09-2025/Hashset/Q1ContainsDuplicates.java
+++ Final revision - 20-09-2025/Hashset/Q1ContainsDuplicates.java
@@ -5,8 +5,10 @@ Check if an array contains duplicate elements
 Example:
 arr = {1, 2, 3, 4, 1}
 Output → true */
+
+import java.util.HashSet;
 public class Q1ContainsDuplicates {
     public static void main(String[] args){
-        
+
     }
 }
