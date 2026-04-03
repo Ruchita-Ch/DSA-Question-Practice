b316349641a68b4ec9b05f45aab231122ba8e9e2
ru2003 <ruchitanchaudhari145@gmail.com>
2026-04-03 17:43:39 +0530

    new changes

--- INterview coding questions/Que3.java
+++ INterview coding questions/Que3.java
@@ -4,18 +4,21 @@ public class Que3 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Amount:");
-        double Amount = sc.nextInt();
+        double Amount = sc.nextDouble();
+        if (Amount <= 0){
+            System.out.println("Error : Invalid amount.");
+            return;
+        }
+        
         double finalpayableamount = 0;
         if(Amount < 1000){
            finalpayableamount = Amount - ( Amount * 5/100);
-        }else if( Amount > 1000  && Amount < 5000){
+        }else if( Amount >= 1000  && Amount < 5000){
             finalpayableamount = Amount - ( Amount * 10/100);
-        }else if( Amount > 5000){
+        }else{
             finalpayableamount = Amount - ( Amount * 15/100);
-        }else if( Amount == 0){
-            System.out.println("Error");
-
         }
+        System.out.println("Final Payable amount after discount is: " + finalpayableamount);
 
     }
 }
