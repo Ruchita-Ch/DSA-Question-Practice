3e78dc5c4ef384c250a50cd09f648765040d3b80
ru2003 <ruchitanchaudhari145@gmail.com>
2026-04-03 17:15:25 +0530

    new changes

--- INterview coding questions/Que3.java
+++ INterview coding questions/Que3.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Que3 {
     public static void main(String[] args){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Amount:");
+        int Amount = sc.nextInt();
+
     }
 }
